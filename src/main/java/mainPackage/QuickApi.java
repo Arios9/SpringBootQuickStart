e5965a747ac337036c9d01b6e0a9b4005b97851b
Arios <ariosssmfm@gmail.com>
2021-06-06 00:45:29 +0300

    first commit

---
+++ src/main/java/mainPackage/QuickApi.java
@@ -0,0 +1,15 @@
+package mainPackage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickApi {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(QuickApi.class, args);
+
+	}
+
+}
